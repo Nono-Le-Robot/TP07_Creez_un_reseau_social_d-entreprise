1d2d2581e9bbf7804827253d0ecda10142a8af99
Nono-Le-Robot <87859199+Nono-Le-Robot@users.noreply.github.com>
2022-07-21 18:57:47 +0200

    fix mac

--- back/controllers/post.controller.js
+++ back/controllers/post.controller.js
@@ -1,4 +1,3 @@
-const { registerCustomQueryHandler } = require("puppeteer");
 const PostModel = require("../models/post.model");
 const UserModel = require("../models/user.model");
 const ObjectId = require("mongoose").Types.ObjectId;
