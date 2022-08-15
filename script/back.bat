0354d045862ab7c333d6f696f439606c2b794854
nono <renaud.sannier@laposte.net>
2022-08-15 18:16:58 +0200

    update probleme login et register

--- script/back.bat
+++ script/back.bat
@@ -1,21 +1,4 @@
 @echo off
 cd ../back
-@echo =========================== Update Package.json : ===========================
-call npm update
-
-
-REM========================= Entrez vos variables ici =======================
-set PORT=5000
-set IDMDB=username:password
-set TOKEN_SECRET=randomstringrandomstringrandomstring
-REM========================= Entrez vos variables ici =======================
-
-
-@echo =============================== Variables env : ==============================
-set PORT
-set IDMDB
-set TOKEN_SECRET
-TIMEOUT /T 5
-cls
 @echo ============================ Start back-end server ===========================
-call npm start server
+call nodemon server
